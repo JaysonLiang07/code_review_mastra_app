db1f79df5270532d32aa661740294daedced36bc
shunge <1075942331@qq.com>
2025-06-16 16:50:45 +0800

    fix

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -4,8 +4,18 @@ import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
+import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
 
 export const mastra = new Mastra({
+  deployer: new CloudflareDeployer({
+		scope: process.env.CLOUDFLARE_ACCOUNT_ID || 'your-cloudflare-scope',
+		projectName: 'code-review-mastra-worker',
+		workerNamespace: 'production',
+		auth: {
+			apiToken: process.env.CLOUDFLARE_API_TOKEN || '',
+			apiEmail: 'liangzhishun000@gmail.com',
+		},
+	}),
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
   storage: new LibSQLStore({
